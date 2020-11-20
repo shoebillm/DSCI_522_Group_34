a94b655a8ee05ca2bdf8e7dccfe64f9200cf8e8a
Kangbo Lu <kblu@ucdavis.edu>
2020-11-20 19:52:57 +0800

    Update comment for download_data.R template

--- src/download_data.R
+++ src/download_data.R
@@ -1,4 +1,6 @@
 # download data with R placeholder
+
+# takes a URL and a local file path as command line arguments which downloads and writes the file to your laptop
 url <- ""
 output_file <- "../data/dataset.csv"
 download.file(url, output_file)
