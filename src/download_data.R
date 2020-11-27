a93dffc486e80ea7dc707b84e6badf1536e2a4c8
mitchiezhao <mitchie.zhao12@gmail.com>
2020-11-27 21:34:45 +0800

    Added error hint in download_data.R

--- src/download_data.R
+++ src/download_data.R
@@ -2,18 +2,29 @@
 # date: 2020-11-20
 
 "This script downloads the dataset and stores it in the designated location.
-Usage: download_data.R --url=<url> --output=<output_file>
+Usage: src/download_data.R --url=<url> --output=<output_file>
 Options:
 --url=<url>               Takes a url from where to download the data as csv
 --output=<output_file>    Takes a local file path to store data
 " -> doc
 
 library(docopt)
+library(RCurl)
 opt <- docopt(doc)
 
 main <- function(opt) {
-    download.file(opt$url, opt$output)
-    cat("Dataset successfully saved in", opt$output)
+# Generate error message if the input url is not valid
+    if (!url.exists(opt$url)) {
+      print("The url is not valid. Please input a valid url for csv data.")
+    } else {
+# Download data and store in designated file when file path is valid
+    if (file.exists(opt$output)) {
+      download.file(opt$url, opt$output)
+      cat("Dataset successfully saved in", opt$output)
+    } else {
+      print("The file path does not exist. Please input a valid file path.")
+    }
+    }
 }
     
 main(opt)
