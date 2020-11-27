a4c08627194a8ecaa192cd7f0e96788deafffe65
mitchiezhao <mitchie.zhao12@gmail.com>
2020-11-27 21:47:31 +0800

    Fixed indentation

--- src/download_data.R
+++ src/download_data.R
@@ -13,18 +13,18 @@ library(RCurl)
 opt <- docopt(doc)
 
 main <- function(opt) {
-# Generate error message if the input url is not valid
+    # Generate error message if the input url is not valid
     if (!url.exists(opt$url)) {
-      print("The url is not valid. Please input a valid url for csv data.")
+        print("The url is not valid. Please input a valid url for csv data.")
     } else {
-# Download data and store in designated file when file path is valid
-    if (file.exists(opt$output)) {
-      download.file(opt$url, opt$output)
-      cat("Dataset successfully saved in", opt$output)
-    } else {
-      print("The file path does not exist. Please input a valid file path.")
-    }
+        # Download data and store in designated file when file path is valid
+        if (file.exists(opt$output)) {
+            download.file(opt$url, opt$output)
+            cat("Dataset successfully saved in", opt$output)
+        } else {
+            print("The file path does not exist. Please input a valid file path.")
+        }
     }
 }
-    
+
 main(opt)
