ef58ed176d99841ef2753fddb1204d0de39fc15f
KangboLu <kblu@ucdavis.edu>
2020-11-28 20:59:33 +0800

    update indentation and usage for scripts

--- src/process_data.R
+++ src/process_data.R
@@ -1,58 +1,38 @@
 # process_data.R
-# author: Kangbo Lu, Siqi Zhou, Mitchie Zhao, Mengyuan Zhu
-#
-# date: 2020-11-25
-#
-# This script downloads the dataset, precesses it and store the processed data in the designated location
-# This script takes 2 arguments:path of input_file,and path of output_file.
-#
-# Usage: Rscript src/process_data.R --input="data/dataset.csv" --output="data/processed/processed.csv"
+# author: Kangbo Lu, Mengyuan Zhu, Mitchie Zhao, Siqi Zhou
+# date: 2020-11-28
+# usage example: Rscript src/process_data.R --input="data/dataset.csv" --output="data/processed/processed.csv"
 
-"This script downloads the dataset, processes and stores it in the designated location.
+"Clean, split, and process the raw gratiffi dataset for eda and analysis.
+
 Usage: process_data.R --input=<input_file> --output=<output_file>
 Options:
---input=<input_file>    Takes a local relative relative filename path to read input data
---output=<output_file>  Takes a local relative relative filename path to store processed data
+--input=<input_file>    Takes a local relative path includes filename path to read input csv data
+--output=<output_file>  Takes a local relative path includes filename to store processed csv data
 " -> doc
 
-# import libraries/packages
-
+# import libraries
 library(docopt)
 library(tidyverse)
-
 opt <- docopt(doc)
 
 # define main function
-
 main <- function(opt) {
-    
-## join relative file paths to absolute file paths
-    
+    # join relative file paths to absolute file paths
     input_file <- file.path(getwd(), opt$input)
     output_file <- file.path(getwd(), opt$output)
-
-## readin data, sparse them with delimetre ";"
     
+    # readin data, tidy columns and select wanted columns
     graffiti <- read_delim(input_file, delim = ";") %>%
-    
-### pre-process the .csv file, transfer the text into lower case and tidy up the spaces
-    
-        rename_with(~ tolower(gsub(" ", "_", .x, fixed = TRUE))) %>%
-    
-### select relevant data in 2 areas of our interest
-    
+        rename_with( ~ tolower(gsub(" ", "_", .x, fixed = TRUE))) %>%
         select(count, geo_local_area) %>%
         filter(geo_local_area == "Downtown" | geo_local_area == "Strathcona") %>%
-    
-### drop null values
-    
         drop_na()
-
-## save processed data
+    
+    # save processed data with the given file path includes filename
     write_delim(graffiti, output_file, delim = ";")
     paste("Processed data saved as", output_file)
 }
 
-# call main function
-
+# call the main function when script is executed
 main(opt)
