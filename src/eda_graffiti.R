65a0668ffb41e2ae7cde33b7136e6dfb80942663
Julie-M-Zhu <zhumengyuan@126.com>
2020-11-28 18:47:49 +0800

    comment the script with thorough description and usage

--- src/eda_graffiti.R
+++ src/eda_graffiti.R
@@ -1,7 +1,18 @@
-# author: UBC MDS Group 34
+# eda_graffiti.R
+#
+# author: UBC MDS Group 34,author: Kangbo Lu, Siqi Zhou, Mitchie Zhao, Mengyuan Zhu
+#
 # date: 2020-11-25
-# Example: Rscript src/eda_graffiti.R --input="data/processed/processed.csv" --output="results/graffiti_eda.png"
-
+#
+# Usage: Rscript src/eda_graffiti.R --input="data/processed/processed.csv" --output="results/graffiti_eda.png"
+#
+## input_file here refers to the pre-precessed data after running process_data.R
+#
+## output_file refers to a .png figure contains the distribution of samples
+#
+## this script uses the violin plot to show the distribution of the count of graffiti per area in 2 regions of Vancouver.
+#
+#
 "This script downloads the dataset and stores it in the designated location.
 Usage: eda_graffiti.R --input=<input_file> --output=<output_file>
 Options:
@@ -9,19 +20,32 @@ Options:
 --output=<output_file>  Takes a local relative filename path to store eda figure
 " -> doc
 
+# import libraries/packages
+
 library(docopt)
 library(tidyverse)
 library(ggplot2)
 
 opt <- docopt(doc)
 
+#define the main function
+
 main <- function(opt) {
-    # join relative file paths to absolute file paths
+    
+## join relative file paths to absolute file paths
+    
     input_file <- file.path(getwd(), opt$input)
     output_file <- file.path(getwd(), opt$output)
 
-    # eda data
+# exploratory data analysis pipeline
+    
+## read-in the pre-processed file after process_data.R
+    
     graffiti <- read_delim(input_file, delim = ";")
+    
+## Use ggplot in R to draw the distribution figure:
+## Distribution of graffiti count by Vancouver region    
+    
     violin_plot <- graffiti %>%
         ggplot(aes(x = geo_local_area, y = count)) +
         geom_violin(trim = FALSE) +
@@ -30,9 +54,10 @@ main <- function(opt) {
         ylab("graffiti count") +
         xlab("Vancouver region")
 
-    # save eda figure
+## save eda figure
     ggsave(output_file, violin_plot)
     paste("ead data figure saved as", output_file)
 }
+# call the main function
 
 main(opt)
